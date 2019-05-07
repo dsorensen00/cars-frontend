da5903a665ea844656fd49fdcada5623d56b1a7a
Nate Reid <reidnate91@Nates-MacBook-Air.local>
2019-05-07 10:24:50 -0600

    fixed home page

---
+++ src/Components/Routing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Forums from './Forums'
+import Home from '../Pages/Home'
+import ForSale from './ForSale'
+import Videos from './Videos'
+import Help from './Help'
+
+export default () => {
+
+    return(
+<Router>
+    <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/forums" component={Forums}/>
+        <Route path="/forsale" component={ForSale}/> 
+        <Route path="/videos" component={Videos}/>
+        <Route path="/help" component={Help}/>
+    </Switch>
+</Router>
+    )}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,6 +9,19 @@ export default() => {
         <h1>My Dream Garage</h1>
         <Login/>
         <Navbar/>
+        <div className="row">
+        
+        <section name="Featured Article" className="col border" >
+            <h1>Featured Article</h1>
+        </section>
+        <section name="About" className="col border">
+            <h1>About</h1>
+        </section>
+       
+        </div>
+        <section name="Featured Vehicles" className="row border">
+            <h1>Featured Vehicles</h1>
+        </section>
      </div>
     );
 }
