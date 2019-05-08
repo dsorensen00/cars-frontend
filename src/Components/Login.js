24413e595cd20e87cf95201fe13915206f99cedd
Nate Reid <reidnate91@Nates-MacBook-Air.local>
2019-05-08 11:12:38 -0600

    added nav to login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Navbar from './Navbar'
 
 
 
@@ -22,6 +23,7 @@ export default() => {
    
   return (
     <>
+    <Navbar/>
       <div className="background">
         <form onSubmit={handleSubmit}  name="login" className="container pad">
           <div className="row justify-content-around" >
